8ce6e4c5b6f4f51d5199c926a72faa3b2b6ac50c
Miles <miles@DESKTOP-CEDMDNJ.localdomain>
2022-02-11 18:24:55 -0500

    added tmdb api calls

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import flask
 import os
 import requests
@@ -7,22 +8,31 @@ app = flask.Flask(__name__)
 @app.route('/')
 
 def index():
+    #list of tmbd move IDS
+    movieIDs = [157336, 15040,37165]
+    wikiLinks = ["https://en.wikipedia.org/wiki/Interstellar_(film)/w/api.php", "https://en.wikipedia.org/wiki/Big_Money_Hustlas/w/api.php", "https://en.wikipedia.org/wiki/The_Truman_Show/w/api.php"]
+
+    #get random movie from list
+    RandomNum = random.randint(1,3)
+    ID = movieIDs[RandomNum]
+    link = wikiLinks[RandomNum]
+
     load_dotenv(find_dotenv())
-    
     TMDB_KEY = os.getenv("TMDB_KEY")
     WIKI_KEY = os.getenv("WIKI_KEY")
-    TMDB_URL = #TODO get this
-    WIKI_URL = #TODO get this
+    TMDB_URL = "https://api.themoviedb.org/3/movie/" + ID + "?api_key=" + TMDB_KEY + "&language=en-US"
+    #WIKI_URL = #TODO get this
 
     #get TMDB
     responseTMDB = requests.get(TMDB_URL)
     responseTMDB_json = responseTMDB.json()
 
-    TMDB_List
-
     try:
-        results = responseTMDB_json["results"]
-        TMDB_List = [results['original_title']] #TODO Add tagline, genres, poster image
+        Title = responseTMDB_json["title"]
+        Tagline = responseTMDB_json["tagline"]
+        Genres = []
+        for genre in responseTMDB_json["genres"]:
+            Genres.append(genre["name"])        
     except KeyError:
         print('TMDB API fail')
     
@@ -32,7 +42,7 @@ def index():
     responseWIKI_json = responseWIKI.json()
 
     Wiki_link
-
+    
     try:
         Wiki_link = responseWIKI_json[] #TODO get the link
     except KeyError:
